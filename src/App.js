4a27966996476728f283a8ba9e5c7769d57a3e88
Kimm <hyunsung.kim@zeals.co.jp>
2020-06-19 19:06:25 +0900

    create team generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    playerName: '',
+    players: ["Akutsu", "Kim", "Igai", "Mackee", "Nguyen", "Mike", "Aaron", "Sagar"],
+    colors: ["Red", "Blue"],
+  }
+
+
+  handleChange = (e) => {
+    this.setState({ playerName: e.target.value });
+  }
+
+  addPlayer = () => {
+    const { playerName, players } = this.state;
+    const newplayer = players.concat(playerName)
+    if (playerName !== "") {
+      this.setState({ players: newplayer, playerName: '' })
+    }
+    this.generateTeam(players)
+  }
+
+  generateTeam = (players) => {
+    let i = players.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = players[i];
+      players[i] = players[j];
+      players[j] = temp;
+    }
+    return players;
+  }
+
+  onKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      this.addPlayer();
+    }
+  }
+
+  render() {
+    const { playerName, players } = this.state
+    return (
+      <div className="App" >
+        <h1>Team Generator</h1>
+        <div className="left" >
+          <input
+            value={playerName}
+            onChange={e => this.handleChange(e)}
+            onKeyDown={this.onKeyPress}
+          />
+          <button
+            className="submit-button"
+            onClick={this.addPlayer}
+          >Submit Button</button>
+        </div>
+        <div className="right">
+          {players.sort().map(player => (
+            <li>{player}</li>
+          ))
+          }
+        </div>
+        <button
+          className="submit-button"
+          onClick={this.generateTeam(players)}
+        >Submit Button</button>
+        {players}
+      </div>
+    );
+  }
 }
 
 export default App;
